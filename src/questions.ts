d261d0d8a5198a228a1c5f89aad0afca2ca2f32d
youngkidwarrior <victor@she.energy>
2023-07-18 14:10:58 -0700

    standarize question contract event names v0.1.1

--- src/questions.ts
+++ src/questions.ts
@@ -1,117 +1,66 @@
 import {
-  Edited as EditedEvent,
   Locked as LockedEvent,
-  RoleAdminChanged as RoleAdminChangedEvent,
-  RoleGranted as RoleGrantedEvent,
-  RoleRevoked as RoleRevokedEvent,
-  Submitted as SubmittedEvent,
-  Unlocked as UnlockedEvent
-} from "../generated/Questions/Questions"
+  QuestionEdited as QuestionEditedEvent,
+  QuestionSubmitted as QuestionSubmittedEvent,
+  Unlocked as UnlockedEvent,
+} from "../generated/Questions/Questions";
 import {
-  Edited,
   Locked,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  Submitted,
-  Unlocked
-} from "../generated/schema"
-
-export function handleEdited(event: EditedEvent): void {
-  let entity = new Edited(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.question = event.params.question
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+  QuestionEdited,
+  QuestionSubmitted,
+  Unlocked,
+} from "../generated/schema";
 
 export function handleLocked(event: LockedEvent): void {
   let entity = new Locked(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
+  );
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
-export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
-  let entity = new RoleAdminChanged(
+export function handleQuestionEdited(event: QuestionEditedEvent): void {
+  let entity = new QuestionEdited(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
+  );
+  entity.tokenId = event.params.tokenId;
+  entity.question = event.params.question;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
-export function handleRoleGranted(event: RoleGrantedEvent): void {
-  let entity = new RoleGranted(
+export function handleQuestionSubmitted(event: QuestionSubmittedEvent): void {
+  let entity = new QuestionSubmitted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
+  );
+  entity.tokenId = event.params.tokenId;
+  entity.question = event.params.question;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
-}
-
-export function handleRoleRevoked(event: RoleRevokedEvent): void {
-  let entity = new RoleRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSubmitted(event: SubmittedEvent): void {
-  let entity = new Submitted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.question = event.params.question
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.save();
 }
 
 export function handleUnlocked(event: UnlockedEvent): void {
   let entity = new Unlocked(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
+  );
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
