0282248a244333256ad4562399fb1506c7b1be65
youngkidwarrior <victor@she.energy>
2023-09-11 16:39:02 -0700

    Create an entity for the Votes token and its contract

--- src/votes.ts
+++ src/votes.ts
@@ -1,3 +1,4 @@
+import { BigInt, Bytes, Address, log } from "@graphprotocol/graph-ts";
 import {
   Approval as ApprovalEvent,
   ApprovalForAll as ApprovalForAllEvent,
@@ -6,8 +7,9 @@ import {
   RoleAdminChanged as RoleAdminChangedEvent,
   RoleGranted as RoleGrantedEvent,
   RoleRevoked as RoleRevokedEvent,
-  Transfer as TransferEvent
-} from "../generated/Votes/Votes"
+  Transfer as TransferEvent,
+  Votes,
+} from "../generated/Votes/Votes";
 import {
   Approval,
   ApprovalForAll,
@@ -16,37 +18,80 @@ import {
   RoleAdminChanged,
   RoleGranted,
   RoleRevoked,
-  Transfer
-} from "../generated/schema"
+  Transfer,
+  Vote,
+  VoteContract,
+} from "../generated/schema";
+
+const ZERO_ADDRESS = Address.fromString(
+  "0x0000000000000000000000000000000000000000"
+);
+
+function getOrCreateContract(address: Address): VoteContract {
+  let contract = VoteContract.load(address);
+
+  if (!contract) {
+    let instance = Votes.bind(address);
+    contract = new VoteContract(address);
+    contract.name = instance.name();
+    contract.symbol = instance.symbol();
+    contract.totalSupply = BigInt.fromI32(0);
+    contract.save();
+  }
+
+  return contract;
+}
+
+function getOrCreateToken(tokenId: BigInt, address: Address): Vote {
+  let id = Bytes.fromI32(tokenId.toI32());
+  let vote = Vote.load(id);
+
+  if (!vote) {
+    let instance = Votes.bind(address);
+    vote = new Vote(id);
+
+    vote.contract = address;
+    let uri = instance.try_tokenURI(tokenId);
+    if (uri.reverted) {
+      log.info("tokenUri reverted", []);
+    } else {
+      vote.uri = uri.value;
+    }
+
+    vote.save();
+  }
+
+  return vote;
+}
 
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
+  );
+  entity.owner = event.params.owner;
+  entity.approved = event.params.approved;
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
   let entity = new ApprovalForAll(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
+  );
+  entity.owner = event.params.owner;
+  entity.operator = event.params.operator;
+  entity.approved = event.params.approved;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleBatchMetadataUpdate(
@@ -54,86 +99,97 @@ export function handleBatchMetadataUpdate(
 ): void {
   let entity = new BatchMetadataUpdate(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._fromTokenId = event.params._fromTokenId
-  entity._toTokenId = event.params._toTokenId
+  );
+  entity._fromTokenId = event.params._fromTokenId;
+  entity._toTokenId = event.params._toTokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleMetadataUpdate(event: MetadataUpdateEvent): void {
   let entity = new MetadataUpdate(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._tokenId = event.params._tokenId
+  );
+  entity._tokenId = event.params._tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
   let entity = new RoleAdminChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
+  );
+  entity.role = event.params.role;
+  entity.previousAdminRole = event.params.previousAdminRole;
+  entity.newAdminRole = event.params.newAdminRole;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoleGranted(event: RoleGrantedEvent): void {
   let entity = new RoleGranted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
+  );
+  entity.role = event.params.role;
+  entity.account = event.params.account;
+  entity.sender = event.params.sender;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoleRevoked(event: RoleRevokedEvent): void {
   let entity = new RoleRevoked(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
+  );
+  entity.role = event.params.role;
+  entity.account = event.params.account;
+  entity.sender = event.params.sender;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+  );
+  let contract = getOrCreateContract(event.address);
+  let vote = getOrCreateToken(event.params.tokenId, event.address);
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.tokenId = event.params.tokenId;
 
-  entity.save()
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  vote.owner = event.params.to;
+  vote.save();
+
+  if (event.params.from == ZERO_ADDRESS) {
+    contract.totalSupply = contract.totalSupply.plus(BigInt.fromI32(1));
+    contract.save();
+  }
+
+  entity.save();
 }
